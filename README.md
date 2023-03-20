64dbabd83503475501acdaa58e101fdfe9edc75d
Alpaca <Alpaca4610>
2023-03-20 22:06:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+<div align="center">
+  <a href="https://v2.nonebot.dev/store"><img src="https://github.com/A-kirami/nonebot-plugin-template/blob/resources/nbp_logo.png" width="180" height="180" alt="NoneBotPluginLogo"></a>
+  <br>
+  <p><img src="https://github.com/A-kirami/nonebot-plugin-template/blob/resources/NoneBotPlugin.svg" width="240" alt="NoneBotPluginText"></p>
+</div>
+
+<div align="center">
+
+# nonebot-plugin-ai-interviewer
+</div>
+
+## 介绍
+- 本插件基于OpenAI的API开发，在nonebot框架下实现一个聊天式的可自定义面试职位的AI辅助面试官。
+- 本插件具有用户识别功能
+## 安装
+
+* 手动安装
+  ```
+  git clone https://github.com/Alpaca4610/nonebot-plugin-ai-interviewer.git
+  ```
+
+  下载完成后在bot项目的pyproject.toml文件手动添加插件：
+
+  ```
+  plugin_dirs = ["xxxxxx","xxxxxx",......,"下载完成的插件路径/nonebot-plugin-ai-interviewer"]
+  ```
+* 使用 pip
+  ```
+  pip install nonebot-plugin-ai-interviewer
+  ```
+
+# 配置文件
+
+在Bot根目录下的.env文件中追加如下内容：
+
+```
+OPENAI_API_KEY = key
+```
+
+可选内容：
+```
+OPENAI_HTTP_PROXY = "http://127.0.0.1:8001"    # 中国大陆/香港IP调用API请使用代理访问api,否则有几率会被封禁
+OPENAI_MODEL_NAME = True   # AI面试官使用的模型名称
+```
+
+
+# 使用方法
+
+- 初始化AI面试官：（注意，命令中的逗号为中文逗号）
+```
+interviewer 公司：XXXX 职位：XXXXX
+```
+- 回答面试官问题
+```
+interviewer 你的回答
+```
+- 停止面试，开始下一轮面试需要重新初始化
+```
+stop
+```
---
+++ nonebot_plugin_ai_interviewer/__init__.py
@@ -0,0 +1,63 @@
+import nonebot
+
+from nonebot import on_command
+from nonebot.params import CommandArg
+from nonebot.adapters.onebot.v11 import Message, MessageSegment
+from nonebot.adapters.onebot.v11 import MessageEvent
+from .config import Config, ConfigError
+from .interviewer import Interviewer
+
+plugin_config = Config.parse_obj(nonebot.get_driver().config.dict())
+
+if not plugin_config.openai_api_key:
+    raise ConfigError("请设置 openai_api_key")
+
+api_key = plugin_config.openai_api_key
+model_id = plugin_config.openai_model_name
+session = {}
+
+chat_request = on_command("interview", block=True, priority=1)
+
+
+@chat_request.handle()
+async def _(event: MessageEvent, msg: Message = CommandArg()):
+    if api_key == "":
+        await chat_request.finish(MessageSegment.text("请先配置openai_api_key"), at_sender=True)
+
+    content = msg.extract_plain_text()
+
+    if content == "" or content is None:
+        await chat_request.finish(MessageSegment.text("内容不能为空！"), at_sender=True)
+
+    if event.get_session_id() not in session:
+        await chat_request.send(MessageSegment.text("没有面试数据，即将开始新一轮的面试"))
+        company = content.split("：")[1].split("，")[0]
+        job = content.split("：")[2]
+        try:
+            session[event.get_session_id()] = Interviewer(api_key=api_key, model_id=model_id, company=company, job=job)
+            res = await session[event.get_session_id()].init_interview()
+        except Exception as error:
+            await chat_request.finish(str(error), at_sender=True)
+        await chat_request.finish(MessageSegment.text(res), at_sender=True)
+
+    else:
+        await chat_request.send(MessageSegment.text("面试官思考中"))
+        try:
+            res = await session[event.get_session_id()].get_ans(content)
+        except Exception as error:
+            await chat_request.finish(str(error), at_sender=True)
+        await chat_request.finish(MessageSegment.text(res), at_sender=True)
+
+
+
+
+clear_request = on_command("stop", block=True, priority=1)
+
+
+@clear_request.handle()
+async def _(event: MessageEvent):
+    try:
+        del session[event.get_session_id()]
+    except Exception as error:
+        await chat_request.finish(str(error), at_sender=True)
+    await clear_request.finish(MessageSegment.text("面试停止！程序将清除你的面试记录"), at_sender=True)
