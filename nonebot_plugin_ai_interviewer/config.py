d2ab01a50ad7a13d99e9bdd975c5ea7928bb6f44
LanQian <877133467@qq.com5499636+chenjunyan@users.noreply.github.com>
2023-03-28 20:02:40 +0800

    增加api代理

--- nonebot_plugin_ai_interviewer/config.py
+++ nonebot_plugin_ai_interviewer/config.py
@@ -5,6 +5,7 @@ from typing import Optional
 class Config(BaseModel, extra=Extra.ignore):
     openai_api_key: Optional[str] = ""
     openai_model_name: Optional[str] = "gpt-3.5-turbo"
+    openai_api_base: Optional[str] = None
     openai_http_proxy: Optional[str] = None
 
 
